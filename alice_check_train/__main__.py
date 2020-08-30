715426b865b5d089dd79ce00c2ac08036dcaaf27
Aznaur <deccer789@gmail.com>
2020-08-30 12:28:05 +0300

    add some variation to Alice speech, fixed 1 bag

--- alice_check_train/__main__.py
+++ alice_check_train/__main__.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from alice_check_train.main import rasp_to_text
@@ -8,7 +9,7 @@ def main():
     key = os.getenv('RASP_KEY')
     station_from = os.getenv('STATION_FROM')
     station_to = os.getenv('STATION_TO')
-    date = os.getenv('DATE')
+    date = datetime.date.today().strftime('%Y-%m-%d')
 
     js = get_rasp(key, station_from, station_to, date)
     filtered = filter_rasp(js['segments'], 60)
